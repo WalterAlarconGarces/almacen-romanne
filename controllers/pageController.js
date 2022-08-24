1114916dd58e51f7acf9b92aabb04680fad44ec2
WalterAlarconGarces <walarcon@gmail.com>
2022-08-24 13:10:14 -0400

    prueba de dirname

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -4,6 +4,8 @@ const data_base = require('../config/database'); //calls database configuration
 exports.IndexPage = async (req,res)=>{
     try {
         //res.render("index", {});
+        console.log("WWWWWWWW")
+        console.log(__dirname)
         res.sendFile(__dirname+'../index.html')
     } catch (error) {
         console.log(error)
